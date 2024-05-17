e7bddbd855919523f2f8fb2ccc7c9b267ba12070
Prechak <benedict.precha@gmail.com>
2024-05-17 14:31:26 +0700

    callbacks ex2

--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,22 @@
 //Exercise #2: At Least Five Function
 
 function atLeastFive(array, operation) {
-  // Start coding here
+  let passStudent = [];
+  for (let i = 0; i < array.length; i++) {
+    operation(array[i], passStudent);
+  }
+
+  if (passStudent.length >= 5) {
+    return "pass";
+  } else {
+    return "not pass";
+  }
+}
+
+function checkPass(score, array) {
+  if (score > 70) {
+    array.push(score);
+  }
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -9,7 +24,14 @@ const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
+console.log(atLeastFive(studentScoresRoom1, checkPass));
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = console.log(
+  `นักเรียนห้องที่ 1 ${atLeastFive(studentScoresRoom1, checkPass)}`
+);
+let scoreRoom2Result = console.log(
+  `นักเรียนห้องที่ 2 ${atLeastFive(studentScoresRoom2, checkPass)}`
+);
+let scoreRoom3Result = console.log(
+  `นักเรียนห้องที่ 3 ${atLeastFive(studentScoresRoom3, checkPass)}`
+);
