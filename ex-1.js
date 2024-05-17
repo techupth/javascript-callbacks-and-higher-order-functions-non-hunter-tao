ac4b3c01536e7a704438eb372cc55742cc0037f6
Prechak <benedict.precha@gmail.com>
2024-05-17 12:52:33 +0700

    (ex-1): javascript callbacks and higher order functions assignment

--- ex-1.js
+++ ex-1.js
@@ -2,11 +2,15 @@
 
 function forEach(array, operation) {
   // Start coding here
+  for (let i = 0; i < array.length; i++) {
+    operation.push(array[i] + 5000);
+  }
 }
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
 // Using `forEach` function here
+forEach(employeeSalaries, newEmployeeSalaries);
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
